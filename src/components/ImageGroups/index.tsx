b47d5d6419dfb429e99c14ced722127538315733
Đoàn Tiến Phát <45306374+tienphat12081208@users.noreply.github.com>
2020-07-19 00:00:11 +0700

    Deploy banner

--- src/components/ImageGroups/index.tsx
+++ src/components/ImageGroups/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import home_image from "../../assets/images/home_image.png";
+import home_image from "../../assets/images/banner-one.jpg";
 
 import "./style.scss";
 
@@ -11,6 +11,20 @@ const ImageGroup = () => {
         className="component-image-groups-image"
         alt="groups"
       />
+      <div className='component-image-groups-information'>
+      <p className="component-image-groups-information-wellcome">WELCOME TO</p>
+      <p className="component-image-groups-information-title">
+        Coloring
+      </p>
+      <p className="component-image-groups-information-title2">
+        Map
+      </p>
+      <div className="component-image-groups-information-line"></div>
+      <div className="component-image-groups-information-instructors">Instructors</div>
+      <div className="component-image-groups-information-detail">
+        TS. NGUYEN THIEN BAO
+      </div>
+      </div>
     </div>
   );
 };
